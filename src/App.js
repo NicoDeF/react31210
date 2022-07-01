199f3d810fd0f7ff606338d0a03b6c9542d988e1
Nico <nicodf87@gmail.com>
2022-06-30 23:01:07 -0300

    5to desafio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+//import 'bootstrap/dist/css/bootstrap.css'
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import NavBar from "./Components/UI/NavBar/NavBar";
 import ItemListCointainer from "./Components/UI/ItemListContainer/ItemListContainer"
 
--- src/Components/UI/ItemListContainer/ItemListContainer.js
+++ src/Components/UI/ItemListContainer/ItemListContainer.js
@@ -1,19 +1,45 @@
 import React from "react";
+import { useEffect, useState } from 'react'
 import ItemCount from "../ItemCount/ItemCount";
+import ItemList from "../ItemList/ItemList";
 import './ItemListContainer.css'
+import { getData } from '../../mocks/fakeApi.js'
 
 
 const ItemListCointainer = ({marca}) => {
 
+  const [productList, setProductList]=useState([])
+  const [loading, setLoading]=useState(true)
+
     const onAdd = (cant) => {
         console.log(`Se agrego ${cant}`)
+
     }
+        const getProducts = async () => {
+            try{
+              const respuesta = await getData
+              setProductList(respuesta)
+            }catch(error){
+              console.log(error)
+            }finally{
+              setLoading(false)
+            }
+          }
+      
+          useEffect(()=>{
+            getProducts()
+          },[])
+    
 
 return(
     <>
-    <h2> Lista de Autos : {marca} </h2>
+    <h1> Lista de Autos : {marca} </h1>
     <ItemCount model='Siroco' stock={5}  onAdd={onAdd}/>
+    {loading ? <p> Loading.. </p> :
+    <ItemList productList={productList}/>
+}
     </>
-)}
+)
+}
 
 export default ItemListCointainer
